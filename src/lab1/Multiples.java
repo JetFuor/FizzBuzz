a5539ae9966c91e6fb4f4f7f7f1c39ad9cfdca54
JetFuor <52444435+JetFuor@users.noreply.github.com>
2025-09-08 10:44:46 -0400

    Task 4.1

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -5,7 +5,7 @@ public class Multiples {
         System.out.println(multiples(1000, 3, 5));
     }
 
-    static int multiples(int n, int a, int b) {
+    public static int multiples(int n, int a, int b) {
         int count = 0;
         for (int i = 1; i < n; i++) {
             boolean divisibleBy3 = i % a == 0;
@@ -16,5 +16,9 @@ public class Multiples {
             }
         }
         return count;
+    }
+
+    static int multiples() {
+
     }
 }
