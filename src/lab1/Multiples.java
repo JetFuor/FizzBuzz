c77dbbb9a2f4aabf7cc78e36ef8dcc8aae38b8dc
JetFuor <52444435+JetFuor@users.noreply.github.com>
2025-09-08 10:31:29 -0400

    Finished 4.1

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,20 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            boolean divisibleBy3 = i % a == 0;
+            boolean divisibleBy5 = i % b == 0;
+
+            if (divisibleBy5 || divisibleBy3) {
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+}
--- test/lab1/MultiplesTest.java
+++ test/lab1/MultiplesTest.java
@@ -5,12 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /*
   Some test cases for Multiples.
  */
+
 public class MultiplesTest {
 
-    // TODO: When you are ready to test task X,
-    //  copy this file into the test folder.
-
-    @Test
+    @org.junit.Test
     public void testFifteen() {
         assertEquals(7,
                 Multiples.multiples(16, 3, 5),
@@ -18,7 +16,7 @@ public class MultiplesTest {
                         " it should be counting are: 3, 5, 6, 9, 10, 12, 15");
     }
 
-    @Test
+    @org.junit.Test
     public void testTen() {
         assertEquals(4,
                 Multiples.multiples(10, 3, 5),
@@ -26,14 +24,14 @@ public class MultiplesTest {
                         " it should be counting are: 3, 5, 6, 9");
     }
 
-    @Test
+    @org.junit.Test
     public void testThousand() {
         assertEquals(466,
                 Multiples.multiples(1000, 3, 5),
                 "Multiples.multiples(1000, 3, 5) is incorrect.");
     }
 
-    @Test
+    @org.junit.Test
     public void testEquals() {
         assertEquals(249,
                 Multiples.multiples(1000, 4, 4),
